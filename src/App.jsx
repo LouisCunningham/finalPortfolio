1d0b8731dd1b4666d59dffcdfa81c3354df26412
LouisCunningham <cunningtechgroup@outlook.com>
2024-09-05 14:39:40 -0400

    update package.json

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import NotFoundPage from './Pages/NotFoundPage';
 const router = createBrowserRouter(
   // creates the route that leads to the HomePage by using index, otherwise path="/" would be used
   createRoutesFromElements(
-  <Route basename="/finalPortfolio" path='/' element={<MainLayout />} >
+  <Route basename="/finalPortfolio/" path='/' element={<MainLayout />} >
     <Route index element={<HomePage /> }/>
     <Route path='/pricing' element={<PricingPage />} />
     <Route path='/blog' element={<BlogPage />} />
