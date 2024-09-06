c6eb592624767859ca8c630d12cbdac1e092ef8b
LouisCunningham <cunningtechgroup@outlook.com>
2024-09-06 12:18:44 -0400

    updated all links

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import blogTemplate from '../assets/blogExample.png';
 import dashTheme from '../assets/dashboardTheme.png';
 import gemClone from '../assets/geminiClone.png';
@@ -7,6 +8,9 @@ import marioMatch from '../assets/marioMatching.png';
 import neuCalc from '../assets/neuCalc.png';
 import qrCode from '../assets/qrCode.png';
 import spaceX from '../assets/spaceXClone.png';
+import minesweeperImage from '../assets/minesweeper.png';
+import relaxationAppImage from '../assets/relax.png';
+import starbucksCloneImage from '../assets/starbucks.png';
 
 
 const Portfolio = () => {
@@ -17,27 +21,52 @@ const Portfolio = () => {
   const portfolioItems = [
     {
       category: 'HTML / CSS',
+      title: 'Starbucks Website Clone',
+      description: 'This project is a pixel-perfect clone of the Starbucks website, built entirely with HTML and CSS to showcase proficiency in modern web design techniques. The clone mirrors the clean and inviting aesthetic of the original Starbucks site, featuring high-quality images, responsive layouts, and a focus on typography and branding. With attention to detail in recreating the navigation, product displays, and promotional banners, this project highlights skills in creating visually appealing and user-friendly static websites. The clone is optimized for performance and cross-browser compatibility, demonstrating strong fundamentals in front-end development.',
+      imageUrl: starbucksCloneImage,
+      link: 'https://codepen.io/LouisC27/pen/ExomrWm',
+    },    
+    {
+      category: 'JavaScript',
+      title: 'Relaxation App',
+      description: 'This project is a calming and interactive relaxation app created using JavaScript, HTML, and CSS. The app provides a soothing user experience with ambient soundscapes, breathing exercises, and peaceful visuals designed to help users unwind and reduce stress. Users can choose from various calming environments, set timers for guided relaxation sessions, and enjoy smooth transitions between scenes. The project showcases the use of JavaScript for dynamic content manipulation, sound control, and responsive design, with a focus on enhancing user well-being through intuitive and aesthetically pleasing interface elements.',
+      imageUrl: relaxationAppImage,
+      link: 'https://codepen.io/LouisC27/pen/oNxZxOV',
+    },
+    {
+      category: 'JavaScript',
+      title: 'Minesweeper Game',
+      description: 'This project is a fully interactive clone of the classic Minesweeper game, built from scratch using JavaScript, HTML, and CSS. The game replicates the traditional Minesweeper experience with a dynamic grid, randomly placed mines, and functionality for flagging potential mine locations. It features responsive design, ensuring smooth gameplay across different devices, and includes logic for handling various difficulty levels, win/lose conditions, and a timer. The project highlights strong JavaScript skills, focusing on game logic, DOM manipulation, and a clean, user-friendly interface.',
+      imageUrl: minesweeperImage,
+      link: 'https://codepen.io/LouisC27/pen/vYGxGPo',
+    },
+    {
+      category: 'HTML / CSS',
       title: 'Space X Website Clone',
       description: 'This project is a fully responsive clone of the official SpaceX website, built to showcase modern web development techniques and design skills. The clone replicates the sleek, futuristic design and dynamic content of the original SpaceX site, including high-quality images, smooth scrolling animations, and interactive elements. It is built using HTML, CSS, and JavaScript, with a focus on clean code, performance optimization, and cross-browser compatibility. The project demonstrates an ability to recreate complex, visually striking websites while maintaining attention to detail and user experience.',
       imageUrl: spaceX,
+      link: 'https://louisc27.github.io/spaceXclone/',
     },
     {
       category: 'Vanilla JS',
       title: 'Neumorphic Calculator',
       description: 'This project is a visually appealing calculator built using HTML, CSS, and JavaScript, featuring a sleek neumorphic design. The interface mimics soft, raised surfaces and subtle shadows, creating a modern and minimalist user experience. The calculator performs basic arithmetic functions with smooth animations and responsive button clicks. The project demonstrates expertise in CSS for implementing the neumorphism design trend, along with JavaScript for handling calculations and user interactions',
       imageUrl: neuCalc,
+      link:'https://codepen.io/LouisC27/pen/wvjjejj',
     },
     {
       category: 'Api Based',
       title: 'QR Code Generator',
       description: 'This project is a web-based QR code generator that utilizes a third-party API to create customized QR codes. Built with HTML, CSS, and JavaScript, the generator allows users to input text or URLs and instantly generate QR codes that can be downloaded or shared. The interface is user-friendly, featuring real-time previews and customization options such as color schemes and size adjustments. By integrating API calls to generate the codes, this project highlights proficiency in working with external APIs, handling asynchronous operations, and building functional, interactive web applications.',
       imageUrl: qrCode,
+      link: 'https://codepen.io/LouisC27/pen/abGYxpw',
     },
     {
       category: 'Vanilla JS',
       title: 'Mario Matching Game',
       description: 'This project is a fun and engaging Mario-themed matching game, built entirely with vanilla JavaScript, HTML, and CSS. The game challenges players to match pairs of iconic Mario characters and items, with smooth card flip animations and visual feedback for correct and incorrect matches. The project includes a timer, score tracker, and restart functionality, all implemented without the use of external libraries. It showcases solid JavaScript skills in DOM manipulation, game logic, and event handling, while the nostalgic Mario theme adds an element of creativity and entertainment to the user experience.',
       imageUrl: marioMatch,
+      link: 'https://louisc27.github.io/marioMatchingGame/',
     },
     {
       category: 'Api Based',
@@ -48,14 +77,16 @@ const Portfolio = () => {
     {
       category: 'Api Based',
       title: 'Gemini Clone',
-      description: 'Wireframe',
+      description: 'The Gemini AI Platform Clone is an advanced AI-driven solution that replicates the core functionalities of the Gemini platform, designed to facilitate seamless and efficient use of artificial intelligence for various business needs. This project focuses on creating a scalable environment for deploying and managing AI models, enabling users to train, evaluate, and deploy machine learning models effortlessly.',
       imageUrl: gemClone,
+      link:'https://louiscunningham.github.io/GeminiCloneApp/',
     },
     {
       category: 'HTML / CSS',
       title: 'Hulu Clone',
       description: 'This project is a replica of the Hulu streaming platforms website, designed to demonstrate proficiency in front-end development and responsive design. The clone features a clean, modern UI with custom navigation menus, dynamic content sections, and engaging hover effects, closely mimicking the original Hulu website experience. Built using HTML, CSS, and JavaScript, the project showcases skills in media queries for device compatibility, as well as optimization techniques for faster load times and smooth user interaction.',
       imageUrl: huluClone,
+      link:'https://louisc27.github.io/huluClone/',
     },
     {
       category: 'React',
@@ -98,7 +129,7 @@ const Portfolio = () => {
           <section key={index} className="lg:flex lg:items-center ">
             <div className="lg:w-1/2 animate-fade-left animate-once animate-duration-[1200ms] animate-delay-[900ms] animate-ease-in-out animate-normal">
             <h2 className="mt-2 text-2xl font-semibold text-gray-800 capitalize pb-4">
-                {item.title}
+                <Link to={item.link} target='_blank'>{item.title}</Link>
               </h2>
               <p className="text-lg tracking-wider text-gray-400 uppercase">{item.description}</p>
               
